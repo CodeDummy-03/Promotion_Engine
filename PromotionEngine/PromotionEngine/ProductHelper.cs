7b5adb634123d22e4e5b47ca189c2c667f65a563
sohampal1995 <sohampal1995@gmail.com>
2020-11-01 19:00:10 +0530

    Adding function structure in test class

--- PromotionEngine/PromotionEngine/ProductHelper.cs
+++ PromotionEngine/PromotionEngine/ProductHelper.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace PromotionEngine
 {
-    class ProductHelper
+    public class ProductHelper
     {
     }
 }
--- PromotionEngine/PromotionEngineTest/ProductHelperTest.cs
+++ PromotionEngine/PromotionEngineTest/ProductHelperTest.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PromotionEngineTest
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PromotionEngine;
+namespace SampelDemoAppForGitRepoTest
 {
-    class ProductHelperTest
+    [TestClass]
+    public class ProductHelperTest
     {
+        public ProductHelper _productHelper;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            //Initialize objects that _productHelper will be accessing and/required for mocking.
+        }
+
+        [TestMethod]
+        public void GetProductNameToPriceMapReturnsDictionaryWithAllProducts()
+        {
+
+        }
+
+        [TestCleanup]
+        public void TestCleaup()
+        {
+            //De-Initialize used objects in TestInitialize
+        }
     }
 }
