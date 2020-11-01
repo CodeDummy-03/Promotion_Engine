ebe885679bf3937327e581bc596f679c97e4f6ae
sohampal1995 <sohampal1995@gmail.com>
2020-11-01 18:50:40 +0530

    Adding test project and test classes for promotion engine tests.

---
+++ PromotionEngine/PromotionEngineTest/PromotionEngineTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PromotionEngineTest
+{
+    [TestClass]
+    public class PromotionEngineTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
