940f56481f3d72f18313041b3782162ccd8ab0c4
sohampal1995 <sohampal1995@gmail.com>
2020-11-01 19:34:37 +0530

    Adding public properties to IPromotionEngine.cs and PromotionEngine.cs so that they can be used in tests.

--- PromotionEngine/PromotionEngine/IPromotionEngines.cs
+++ PromotionEngine/PromotionEngine/IPromotionEngines.cs
@@ -1,8 +1,17 @@
 ﻿
+using System.Collections.Generic;
+
 namespace PromotionEngine
 {
     public interface IPromotionEngines
     {
+        public int TotalNoOfProductsBought { get; set; }
+        public int NoOfProductABought { get; set; }
+        public int NoOfProductBBought { get; set; }
+        public int NoOfProductCBought { get; set; }
+        public int NoOfProductDBought { get; set; }
+        public Dictionary<string, int> ProductNameToPriceMap { get; set; }
+
         int GetTotalPriceForCheckOut();
     }
 }
--- PromotionEngine/PromotionEngine/PromotionEngines.cs
+++ PromotionEngine/PromotionEngine/PromotionEngines.cs
@@ -1,9 +1,16 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace PromotionEngine
 {
     public class PromotionEngines : IPromotionEngines
     {
+        public int TotalNoOfProductsBought { get; set; }
+        public int NoOfProductABought { get; set; }
+        public int NoOfProductBBought { get; set; }
+        public int NoOfProductCBought { get; set; }
+        public int NoOfProductDBought { get; set; }
+        public Dictionary<string, int> ProductNameToPriceMap { get; set; }
+
         static void Main(string[] args)
         {
             
